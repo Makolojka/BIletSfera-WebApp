0ffe990d27c440dc8f75b8acd9db2fb012ae9bfb
Marcin Król <101599455+Makolojka@users.noreply.github.com>
2023-07-12 14:22:56 +0200

    Event details responsivity update
    Changed:
    - detail behaviours changed,
    - page is now more responsive

--- src/app/components/event-detail/event-detail.component.ts
+++ src/app/components/event-detail/event-detail.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
-
+import {Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild} from '@angular/core';
+import * as $ from 'jquery';
+import {ViewportScroller} from "@angular/common";
 @Component({
   selector: 'event-detail',
   templateUrl: './event-detail.component.html',
   styleUrls: ['./event-detail.component.css']
 })
-export class EventDetailComponent {
+export class EventDetailComponent{
+  constructor(private viewportScroller: ViewportScroller) {}
+
+  scrollTo(id: string) {
+    this.viewportScroller.scrollToAnchor(id);
+  }
+  isScreenSmall = false;
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: any) {
+    this.isScreenSmall = window.innerWidth < 768;
+    console.log('Small window');
+  }
+  // // @ts-ignore
+  // @ViewChild('stickyMenu') menuElement: ElementRef ;
+  //
+  // sticky: boolean = false;
+  // elementPosition: any;
+  //
+  // ngOnInit() {
+  // }
+  //
+  // ngAfterViewInit(){
+  //   this.elementPosition = this.menuElement.nativeElement.offsetTop;
+  // }
+  //
+  // @HostListener('window:scroll', ['$event'])
+  // handleScroll(){
+  //   const windowScroll = window.pageYOffset;
+  //   if(windowScroll >= this.elementPosition){
+  //     this.sticky = true;
+  //   } else {
+  //     this.sticky = false;
+  //   }
+  // }
 
 }
