c6a9fe3b51d2a523d8ebcc52ea2a0ce8b0e52197
Marcin Król <101599455+Makolojka@users.noreply.github.com>
2023-10-10 18:25:36 +0200

    Active events styles
    Added:
    - active event panel styles

--- src/app/components/event-manager/event-manager.component.ts
+++ src/app/components/event-manager/event-manager.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-event-manager',
   templateUrl: './event-manager.component.html',
   styleUrls: ['./event-manager.component.css']
 })
-export class EventManagerComponent {
+export class EventManagerComponent implements OnInit{
+  activeEventsVisible = false;
+  completedEventsVisible = false;
+  reportsVisible = false;
+  eventCreationVisible = false;
 
+  activePanel = ''; // Track the active panel
+  public image: string = '././assets/img/sopot.jpg';
+
+  ngOnInit() {
+    this.activeEventsVisible = true;
+  }
+  showActiveEvents() {
+    this.resetVisibility();
+    this.activeEventsVisible = true;
+    this.activePanel = 'activeEvents';
+  }
+
+  showCompletedEvents() {
+    this.resetVisibility();
+    this.completedEventsVisible = true;
+    this.activePanel = 'completedEvents';
+  }
+
+  showReports() {
+    this.resetVisibility();
+    this.reportsVisible = true;
+    this.activePanel = 'reports';
+  }
+
+  showEventCreation() {
+    this.resetVisibility();
+    this.eventCreationVisible = true;
+    this.activePanel = 'eventCreation';
+  }
+
+  private resetVisibility() {
+    this.activeEventsVisible = false;
+    this.completedEventsVisible = false;
+    this.reportsVisible = false;
+    this.eventCreationVisible = false;
+  }
 }
