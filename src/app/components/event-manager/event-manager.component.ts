deb20a2c93588d6388068100e3a059aebcc12339
Marcin Król <101599455+Makolojka@users.noreply.github.com>
2023-10-13 18:42:19 +0200

    Creator panel + fixes
    Added:
    - creator panel
    
    Fixes:
    - categories input date style fixes

--- src/app/components/event-manager/event-manager.component.ts
+++ src/app/components/event-manager/event-manager.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ScaleType} from "@swimlane/ngx-charts";
+import {Time} from "@angular/common";
 
 @Component({
   selector: 'app-event-manager',
@@ -13,8 +14,28 @@ export class EventManagerComponent implements OnInit{
 
   activePanel = ''; // Track the active panel
   public image: string = '././assets/img/sopot.jpg';
-  areEventsPresent: boolean = true;
 
+  // Event creator
+  stepNumber: number = 1;
+  basicInfoVisible = false;
+  locationVisible = false;
+  posterVisible = false;
+  additionalInfoVisible = false;
+  detailsVisible = false;
+  artistsVisible = false;
+
+  // Form data
+  eventName: string = '';
+  startDate: Date = new Date();
+  startDateTime: string = '';
+  endDate: Date = new Date();
+  endDateTime: string = '';
+
+  selectedCategories: string[] = [];
+  selectedSubCategories: string[] = [];
+
+
+  areEventsPresent: boolean = true;
   saleData =  [
     {
       "name": "Bilety",
@@ -64,9 +85,43 @@ export class EventManagerComponent implements OnInit{
   };
 
   ngOnInit() {
-    // this.activeEventsVisible = true;
-    this.reportsVisible = true;
+    this.activeEventsVisible = true;
+    // this.eventCreationVisible = true;
   }
+
+  toggleCategory(type: string, value: string) {
+    if(type==='category'){
+      const index = this.selectedCategories.indexOf(value);
+      if (index !== -1) {
+        // Category exists, remove it
+        this.selectedCategories.splice(index, 1);
+      } else {
+        // Category does not exist, add it
+        this.selectedCategories.push(value);
+      }
+    }
+    else if(type==='subCategory'){
+      const index = this.selectedSubCategories.indexOf(value);
+      if (index !== -1) {
+        // Category exists, remove it
+        this.selectedSubCategories.splice(index, 1);
+      } else {
+        // Category does not exist, add it
+        this.selectedSubCategories.push(value);
+      }
+    }
+
+    console.log(this.selectedCategories)
+    console.log(this.selectedSubCategories)
+  }
+
+  isCategorySelected(category: string) {
+    return this.selectedCategories.includes(category);
+  }
+  isSubCategorySelected(subCategory: string) {
+    return this.selectedSubCategories.includes(subCategory);
+  }
+
   showActiveEvents() {
     this.resetVisibility();
     this.activeEventsVisible = true;
@@ -83,11 +138,80 @@ export class EventManagerComponent implements OnInit{
     this.resetVisibility();
     this.eventCreationVisible = true;
     this.activePanel = 'eventCreation';
+    this.basicInfoVisible = true;
   }
 
   private resetVisibility() {
     this.activeEventsVisible = false;
     this.reportsVisible = false;
     this.eventCreationVisible = false;
+  }
+
+  toggleCreatorPanel(panelName: string) {
+    // Reset visibility for all panels
+    this.basicInfoVisible = false;
+    this.locationVisible = false;
+    this.posterVisible = false;
+    this.additionalInfoVisible = false;
+    this.detailsVisible = false;
+    this.artistsVisible = false;
+
+    // Set the visibility for the selected panel
+    switch (panelName) {
+      case 'basicInfo':
+        this.basicInfoVisible = true;
+        this.locationVisible = false;
+        this.posterVisible = false;
+        this.additionalInfoVisible = false;
+        this.detailsVisible = false;
+        this.artistsVisible = false;
+        this.stepNumber = 1;
+        break;
+      case 'location':
+        this.basicInfoVisible = false;
+        this.locationVisible = true;
+        this.posterVisible = false;
+        this.additionalInfoVisible = false;
+        this.detailsVisible = false;
+        this.artistsVisible = false;
+        this.stepNumber = 2;
+        break;
+      case 'poster':
+        this.basicInfoVisible = false;
+        this.locationVisible = false;
+        this.posterVisible = true;
+        this.additionalInfoVisible = false;
+        this.detailsVisible = false;
+        this.artistsVisible = false;
+        this.stepNumber = 3;
+        break;
+      case 'additionalInfo':
+        this.basicInfoVisible = false;
+        this.locationVisible = false;
+        this.posterVisible = false;
+        this.additionalInfoVisible = true;
+        this.detailsVisible = false;
+        this.artistsVisible = false;
+        this.stepNumber = 4;
+        break;
+      case 'details':
+        this.basicInfoVisible = false;
+        this.locationVisible = false;
+        this.posterVisible = false;
+        this.additionalInfoVisible = false;
+        this.detailsVisible = true;
+        this.artistsVisible = false;
+        this.stepNumber = 5;
+        break;
+      case 'artists':
+        this.basicInfoVisible = false;
+        this.locationVisible = false;
+        this.posterVisible = false;
+        this.additionalInfoVisible = false;
+        this.detailsVisible = false;
+        this.artistsVisible = true;
+        this.stepNumber = 6;
+        break;
+    }
   }
 }
